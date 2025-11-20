f8149c8b8d5146d2dfd6c0141fe073f934e026f7
dandrichest <dandrichest@gmail.com>
2025-11-20 13:09:44 +0100

    user update

--- models/user.js
+++ models/user.js
@@ -11,9 +11,13 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 UserSchema.methods.comparePassword = function (candidate) {
